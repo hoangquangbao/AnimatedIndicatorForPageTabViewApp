7e9ca010d3076a0a3dd4d51277100546839ab5bd
Quang Bao <hoangquangbao.developer@gmail.com>
2022-07-13 09:39:28 +0700

    Get offset

--- AnimatedIndicatorForPageTabView/Home.swift
+++ AnimatedIndicatorForPageTabView/Home.swift
@@ -13,6 +13,9 @@ struct Home: View {
     //use your own tabs here...
     var colors : [Color] = [.red, .blue, .pink, .purple]
     
+    //offset...
+    @State var offset: CGFloat = 0
+    
     var body: some View {
         
         //tabview has problem in ignoring top and bottom edge...
@@ -21,10 +24,47 @@ struct Home: View {
             TabView {
                 ForEach(colors.indices, id:\.self) { index in
                     
-                    colors[index]
+                    if index == 1 {
+                        colors[index]
+                            .overlay(
+                                
+                                //getometry Reader
+                                GeometryReader{ proxy -> Color in
+                                    
+                                    let minX = proxy.frame(in: .global).minX
+                                    
+                                    DispatchQueue.main.async {
+                                        withAnimation(.default) {
+                                            self.offset = -minX
+                                            print(offset)
+                                        }
+                                    }
+                                    
+                                    return Color.clear
+                                }
+                                .frame(width: 0, height: 0)
+                                ,alignment: .leading
+                            )
+                    } else {
+                        colors[index]
+                    }
                 }
             }
-            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+            .overlay (
+                
+                //Animated Indicators...
+                HStack(spacing: 15) {
+                    ForEach(colors.indices, id: \.self){ index in
+                        Capsule()
+                            .fill(Color.white)
+                            .frame(width: 7, height: 7)
+                    }
+                }
+                    .padding(.bottom, UIApplication.shared.windows.first?.safeAreaInsets.bottom)
+                    .padding(.bottom, 10)
+                ,alignment: .bottom
+            )
         }
         .ignoresSafeArea()
     }
